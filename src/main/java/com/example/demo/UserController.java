b5a1b0a736e2bcd18345fc7a7e55c6274eaf961f
Vamshi013 <sprintbootraining@gmail.com>
2022-01-18 23:30:24 +0530

    added delete method

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.example.demo.entity.User;
 
@@ -14,10 +15,14 @@ public class UserController {
 		System.out.println("testing");
 		return "testing";
 	}
-	@PostMapping("/user")
+	@PostMapping("/user") // create 
 	void createUser(@RequestBody User user){
 		System.out.println(user.getName());
 		
+	}
+	
+	@DeleteMapping("/user")
+	void deleteuser() {
 	}
 
 }
