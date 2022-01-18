c2556deef8864cbd158e3fea799b6fc6b059a039
Vamshi013 <sprintbootraining@gmail.com>
2022-01-19 02:57:36 +0530

    Added new things

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.example.demo.entity.User;
+import com.example.demo.service.IUserService;
 
 @RestController
 public class UserController {
+	@Autowired
+	IUserService userService;
 	@GetMapping("/user")
 	String getUser() {
 		System.out.println("testing");
@@ -18,6 +22,7 @@ public class UserController {
 	@PostMapping("/user") // create 
 	void createUser(@RequestBody User user){
 		System.out.println(user.getName());
+		userService.saveUser(user);
 		
 	}
 	
