df8a42efa8a9d427ff5f755438af5e622d6b2727
Vamshi013 <sprintbootraining@gmail.com>
2022-01-20 15:31:12 +0530

    MySql changes

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
@@ -15,9 +19,13 @@ public class UserController {
 	@Autowired
 	IUserService userService;
 	@GetMapping("/user")
-	String getUser() {
-		System.out.println("testing");
-		return "testing";
+	Iterable<User> getUser() {
+		
+		return userService.getUsers();
+	}
+		@GetMapping("/user/{id}")
+		Optional<User>getUsers(@PathVariable("id") Integer id){
+			return userService.getUser(id);
 	}
 	@PostMapping("/user") // create 
 	void createUser(@RequestBody User user){
